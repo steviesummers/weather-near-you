0beaa99964ccf04dbdcfe0e71364d4656245fca5
steviesummers <steviesummerss@gmail.com>
2023-11-06 15:18:31 -0800

    connected API to my javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 var APIKey = "b45ee8c01e0f5a210593d03069c26e1f";
 
+var cityInput = document.querySelector(".city-input");
+var searchButton = document.querySelector(".search-btn");
+
+var getCityCoordinates = () => {
+    var cityName = cityInput.value.trim();
+    if(!cityName) return;
+    var GEOCODING_API_URL = 'http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${APIKey}';
+
+    fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
+console.log(data)
+    })
+
+}
+searchButton.addEventListener("click", getCityCoordinates);
+
 api.openweathermap.org/data/2.5/weather?q={LosAngeles}&appid:{APIkey}
