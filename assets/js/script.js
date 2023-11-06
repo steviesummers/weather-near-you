14c12e0828eae76694cf92820f4e7aa6612fdb33
steviesummers <steviesummerss@gmail.com>
2023-11-05 16:08:58 -0800

    pagadjusted borders and display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,3 @@
 var APIKey = "b45ee8c01e0f5a210593d03069c26e1f";
 
-api.openweathermap.org/data/2.5/weather?q={Los ,Anegels}&appid:{APIkey}
+api.openweathermap.org/data/2.5/weather?q={LosAngeles}&appid:{APIkey}
